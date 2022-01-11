e39492ba570e8d0a3e5eeb19b9202dc56c5dfbf5
massaoudi youssef <massaoudiyou@localhost.localdomain>
2022-01-11 10:18:06 +0100

    StringCalculator : Refactoring step 1

--- StringCalculator/src/main/java/com/yma/calculator/StringCalculatorForStep1.java
+++ StringCalculator/src/main/java/com/yma/calculator/StringCalculatorForStep1.java
@@ -1,10 +1,8 @@
 package com.yma.calculator;
 
-import java.util.Arrays;
-
 public class StringCalculatorForStep1 implements StringCalculator {
     /**
-     * Sum two numbers  separated by commas contained in the given numbers
+     * Sum two numbers separated by commas contained in the given numbers
      *
      * @param numbers
      * @return
@@ -15,12 +13,11 @@ public class StringCalculatorForStep1 implements StringCalculator {
             return 0;
         }
 
-        String[] numbersArray = numbers.split(",");
+        String[] numbersArray = Utils.explode(numbers, ",");
         if (numbersArray.length > 2) {
             return 0;
         }
-        return Arrays.stream(numbersArray)
-                .map(String::trim).mapToInt(Integer::parseInt).sum();
-
+        int[] intsArray = Utils.convert(numbersArray);
+        return Utils.sum(intsArray);
     }
 }
---
+++ StringCalculator/src/main/java/com/yma/calculator/Utils.java
@@ -0,0 +1,42 @@
+package com.yma.calculator;
+
+import java.util.Arrays;
+
+public class Utils {
+
+    private Utils() {
+        // private constructor
+    }
+
+    /**
+     * Sum numbers of the given array
+     *
+     * @param ints
+     * @return
+     */
+    public static int sum(int[] ints) {
+        return Arrays.stream(ints).sum();
+    }
+
+    /**
+     * Extract numbers separated by delimiter from the given string
+     *
+     * @param strings
+     * @param delimiter
+     * @return
+     */
+    public static String[] explode(String strings, String delimiter) {
+        return strings.split(delimiter);
+    }
+
+    /**
+     * Convert the given string array to int array
+     *
+     * @param numbers
+     * @return
+     */
+    public static int[] convert(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(String::trim).mapToInt(Integer::parseInt).toArray();
+    }
+}
