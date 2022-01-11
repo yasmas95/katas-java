3b113344c3f617463a22ef2a3c273dda5f535a6e
massaoudi youssef <massaoudiyou@localhost.localdomain>
2022-01-11 10:04:45 +0100

    SringCalculator : Implemente step 1

--- StringCalculator/src/main/java/com/yma/calculator/StringCalculatorForStep1.java
+++ StringCalculator/src/main/java/com/yma/calculator/StringCalculatorForStep1.java
@@ -1,7 +1,26 @@
 package com.yma.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculatorForStep1 implements StringCalculator {
+    /**
+     * Sum two numbers  separated by commas contained in the given numbers
+     *
+     * @param numbers
+     * @return
+     */
+    @Override
     public int Add(String numbers) {
-        return 1;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbersArray = numbers.split(",");
+        if (numbersArray.length > 2) {
+            return 0;
+        }
+        return Arrays.stream(numbersArray)
+                .map(String::trim).mapToInt(Integer::parseInt).sum();
+
     }
 }
