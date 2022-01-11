06e868ffbc0702aec747b18dbd4b9e1aa2109f0e
massaoudi youssef <massaoudiyou@localhost.localdomain>
2022-01-11 12:10:44 +0100

    StringCalculator : Add unit test for step 5

---
+++ StringCalculator/src/main/java/com/yma/calculator/StringCalculatorForStep5.java
@@ -0,0 +1,34 @@
+package com.yma.calculator;
+
+import java.util.Arrays;
+
+public class StringCalculatorForStep5 implements StringCalculator {
+
+    /**
+     * Sum numbers separated by delimiter in the first line contained in the given numbers
+     * - The given string contains multiline
+     * - With a negative numbers will throw an exception
+     *
+     * @param numbers
+     * @return
+     */
+    @Override
+    public int Add(String numbers) {
+
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        if (numbers.contains(",\n")) {
+            throw new CalculatorException(String.format("The %s is not ok", numbers));
+        }
+        String delimiter = ",";
+        String[] numbersArray = Utils.explode(numbers, "\n");
+
+        if (numbersArray[0].contains("//")) {
+            delimiter = String.valueOf(numbersArray[0].charAt(numbersArray[0].length() - 1));
+            numbersArray = Arrays.copyOfRange(numbersArray, 1, numbersArray.length);
+        }
+
+        return Utils.sumMultiLine(numbersArray, delimiter);
+    }
+}
